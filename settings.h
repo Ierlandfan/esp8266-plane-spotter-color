5737f7c0fa1a6b46525418f8143e47a4745f06d4
Ierlandfan <mac_che@hotmail.com>
2017-07-04 08:33:02 +0200

    Update settings.h

--- settings.h
+++ settings.h
@@ -23,20 +23,56 @@ SOFTWARE.
 See more at https://blog.squix.org
 
 
+
+
+#define PIN_D0 16
+#define PIN_D1  5
+#define PIN_D2  4 //TOUCH_CS
+#define PIN_D3  0 //TFT_DC
+#define PIN_D4  2
+#define PIN_D5 14 // SCLK
+#define PIN_D6 12 // MISO
+#define PIN_D7 13 // MOSI
+#define PIN_D8 15 //TFT_CS
+#define PIN_D9  3
+#define PIN_D10 1
+#define USE_HW_CS
+
 */
+
+#define PIN_D0 16
+#define PIN_D1  5
+#define PIN_D2  4 //TOUCH_CS
+#define PIN_D3  0 //TFT_DC
+#define PIN_D4  2
+#define PIN_D5 14 // SCLK
+#define PIN_D6 12 // MISO
+#define PIN_D7 13 // MOSI
+#define PIN_D8 15 //TFT_CS
+#define PIN_D9  3
+#define PIN_D10 1
+#define USE_HW_CS
+
+#define TOUCH_CS  PIN_D2 // XPT2046 chip select
+#define TFT_CS  PIN_D8 // TFT chip select
+#define TFT_DC PIN_D3 // TFT DC
+#define LED_PIN PIN_D0 // LED not used
+
 
 // Needed for loading the map. If you want to be save better get your own key here:
 // https://developer.mapquest.com/plan_purchase/steps/business_edition/business_edition_free/register
-#define MAP_QUEST_API_KEY "7IbGOnKaynIWUj9Qhd5lGWxAV7UQc9Uz"
+#define MAP_QUEST_API_KEY "r19I8UVBfwIkmE4EZR9S6yMR43eMiRDZ"
 
 // Need for loading map by google static map api. If you want to be save better create one here
 // https://developers.google.com/maps/documentation/static-maps/get-api-key?hl=de
-#define GOOGLE_API_KEY "AIzaSyCOERMOCjVexESL7bV8ik4fhNSQZBD8Dlk"
+#define GOOGLE_API_KEY "AIzaSyBw0G8jCBry0IATNmysuyPd2fBblndS3jU"
 
-#define GFXFONT 1
+#define MAP_ZOOM 10 
+#define MAP_WIDTH 480
+#define MAP_HEIGHT 252 // Todo -- Show METAR/TAF info when no planes in sight.
 
-#define MAP_ZOOM 10 //11
-#define MAP_WIDTH 320
-#define MAP_HEIGHT 200
+
+// How many pixels outside the visible map should planes be requested
+#define MAP_REQUEST_MARGIN 40
 
 
